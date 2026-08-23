3484773ab828fab9638d35f36408fe7b8e4a576f
Jiusi Zheng <155141970+JiusiZheng@users.noreply.github.com>
2026-08-23 22:07:16 +0200

    Add files via upload

--- s2u-L2A.py
+++ s2u-L2A.py
@@ -63,12 +63,11 @@ def torch_load_safe(*args, **kwargs):
     return torch.load_orig(*args, **kwargs)
 torch.load = torch_load_safe
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_clusters', type=int, choices=[50, 100, 200], default=200)
 parser.add_argument('--model', type=str, default='hubert-base-ls960')
-parser.add_argument('--datadir', type=str, default='/vol/das-nobackup/users/jzheng/Database/datasets/VCTK-Corpus-0/VCTK-Corpus-0')
-parser.add_argument('--outdir', type=str, default='/vol/tensusers8/jzheng/T8_Exp/PSST_1/features/VCTK')
+parser.add_argument('--datadir', type=str, default='/vol/das-nobackup/users/jzheng/Database/datasets/l2arctic_release_v5/l2arctic_release_v5')
+parser.add_argument('--outdir', type=str, default='/vol/das-nobackup/users/jzheng/PSST_1/UUVC/features/L2A')
 parser.add_argument('--with_pitch_unit', action='store_true')
 parser.add_argument('--VCTK', action='store_true')
 
@@ -83,27 +82,32 @@ if args.with_pitch_unit:
 #        need_f0=False
     ).cuda()
 
-processed = [p.stem for p in Path(args.outdir).glob(f'*-unit.npy')]
+# processed = [p.stem for p in Path(args.outdir).glob(f'*-unit.npy')]
+# print('###we are here?',processed)
+# exit()
 transforms_16k = {
     22050: torchaudio.transforms.Resample(22050, 16000).cuda(),
     24000: torchaudio.transforms.Resample(24000, 16000).cuda(),
-    48000: torchaudio.transforms.Resample(48000, 16000).cuda()
+    48000: torchaudio.transforms.Resample(48000, 16000).cuda(),
+    44100: torchaudio.transforms.Resample(44100, 16000).cuda()
 }
 transforms_22k = {
     24000: torchaudio.transforms.Resample(24000, 22050).cuda(),
     16000: torchaudio.transforms.Resample(16000, 22050).cuda(),
-    48000: torchaudio.transforms.Resample(48000, 22050).cuda()
+    48000: torchaudio.transforms.Resample(48000, 22050).cuda(),
+    44100: torchaudio.transforms.Resample(44100, 22050).cuda()
 }
 
 wavfiles = [p for p in Path(args.datadir).rglob('*.wav')] + [p for p in Path(args.datadir).rglob('*.flac')]
 if args.VCTK:
     wavfiles = [p for p in wavfiles if '_mic1' in str(p)]
 
-
-for f in tqdm(wavfiles):
+for f in tqdm(wavfiles,unit='file'):
 #    if f.stem + '-unit' in processed:
 #        continue
     wav, sr = torchaudio.load(str(f))
+    # print('we are here??',sr) #44100
+    # exit() #yes
     wav = wav.cuda()
     wav_16k = wav
     if sr != 16000:
@@ -114,6 +118,8 @@ for f in tqdm(wavfiles):
         if sr not in transforms_22k:
             continue
         wav = transforms_22k[sr](wav)
+    # print('we are here??')
+    # exit()
 #    try:
     mels, energy = mel_spectrogram(wav, 1025, 80, 22050, 256, 1024, 0, 8000, return_energy=True)
     mels, energy = mels.cpu().numpy()[0].T, energy.cpu().numpy()[0]
@@ -122,15 +128,22 @@ for f in tqdm(wavfiles):
         units = encoded["units"].cpu().numpy()
         f0 = encoded["f0"].cpu().numpy()
         formants = get_formants_world(wav_16k.cpu().numpy()[0], rate=16000)
-        # print('we are here, f0, formants', f0.shape, '\n', formants)
-        # exit()
 #    except:
 #        print (f.stem)
 #        traceback.print_exc()
 #        continue
+    
     name = f.stem
+    speaker = f.parent.parent.name  
+    # print(speaker,name)
+    name = name.replace("arctic", speaker)
+    # print(new_name)
+    # exit()
+    
     if args.VCTK:
         name = name.replace('_mic1', '')
+    # print('###',os.path.join(args.outdir, name + '-mel.npy'))
+    # exit()
     np.save(os.path.join(args.outdir, name + '-mel.npy'), mels)
     np.save(os.path.join(args.outdir, name + '-E.npy'), energy)
     torchaudio.save(os.path.join(args.outdir, name + '.wav'), wav_16k.cpu(), 16000)
@@ -138,4 +151,3 @@ for f in tqdm(wavfiles):
         np.save(os.path.join(args.outdir, name + '-unit.npy'), units)
         np.save(os.path.join(args.outdir, name + '-f0.npy'), f0)
         np.save(os.path.join(args.outdir, name + '-formants.npy'), formants)
-
