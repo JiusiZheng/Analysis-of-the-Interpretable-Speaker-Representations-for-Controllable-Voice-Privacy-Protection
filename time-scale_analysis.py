215056a4c4634a28022e9a7863b3409b7cb1dc4a
Jiusi Zheng <155141970+JiusiZheng@users.noreply.github.com>
2026-08-24 09:47:36 +0200

    Update time-scale_analysis.py

--- time-scale_analysis.py
+++ time-scale_analysis.py
@@ -1,130 +1,3 @@
-# import torch
-# import torch.nn.functional as F
-# import librosa
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import os
-# from data.dataset import mel_spectrogram
-# import torchaudio
-# from pathlib import Path
-
-# class Config:
-#     sr = 22050
-#     n_fft = 1024
-#     hop_length = 256
-#     n_mels = 80
-
-# cfg = Config()
-
-
-# def smooth(x, win_size):
-#     org = x # B, d, T
-#     # print('111:',x.shape) 
-#     # exit()
-#     x = x.squeeze(0)
-#     T = x.shape[-1]  # ← 记录原始长度
-    
-#     N = win_size
-#     odd = N - 2 * (N // 2)
-    
-#     n = torch.arange(N).float()
-#     hann = torch.sin(torch.pi * (n + 0.5 * (1 + odd)) / (N + odd)) ** 2
-#     hann = hann / ((N + 1) // 2) ** 0.5
-#     kernel = hann.unsqueeze(0).unsqueeze(0).expand(x.shape[0], 1, N)
-    
-#     pad = N // 2
-    
-#     x_padded = F.pad(x.unsqueeze(0), (pad, pad), mode='constant', value=0)
-#     mel_component = F.conv1d(x_padded, weight=kernel, groups=x.shape[0], padding=0)
-#     # print(mel_component.shape, 'T:', T)
-#     # mel_component = mel_component[:, :, :T]  # ← 裁剪到原始长度
-    
-#     stride = (N + 1) // 2
-#     mel_downsample = mel_component[:, :, stride//2::stride]
-    
-#     mel = torch.zeros_like(mel_component)  # 现在长度就是T了
-#     mel[:, :, stride//2::stride] = mel_downsample
-    
-#     mel_padded = F.pad(mel, (pad, pad), mode='constant', value=0)
-#     mel_component = F.conv1d(mel_padded, weight=kernel, groups=x.shape[0], padding=0)
-#     # mel_component = mel_component[:, :, :T]  # ← 同样裁剪
-    
-#     mel_component = mel_component.squeeze(0) # d, T
-#     # print('mel_component shape:', mel_component.shape, 'x shape:', x.shape)
-#     # exit()
-#     assert mel_component.shape == x.shape, f"{mel_component.shape} vs {x.shape}"
-#     return mel_component
-    
-
-
-# wav_paths = [
-#     "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_1.0/03_src_e--03_ref_e.wav",
-#     "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_0.25/03_src_e--03_ref_e.wav"
-# ]
-
-# # win_sizes = [129, 13, 5]  # low / mid-low / mid-high
-# labels = ['Mel1', 'Mel2', 'Mel3', 'Mel4']
-
-# output_dir = "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/decompose_figs"
-# os.makedirs(output_dir, exist_ok=True)
-
-# # =========================
-# # Process each sample
-# # =========================
-# for idx, path in enumerate(wav_paths):
-#     p = Path(path)
-#     wav, sr = torchaudio.load(str(path))
-#     if sr != 22050:
-#         exit(f"Sample {idx+1}: Unsupported sample rate {sr}. Please resample to 22050 Hz.")
-#     mel, energy = mel_spectrogram(wav, 1025, 80, 22050, 256, 1024, 0, 8000, return_energy=True)
-
-#     ### Get different time-scale features in the mel-spectrogram
-#     mel_1 = smooth(mel, win_size=129) #low frequency component
-#     mel_2 = smooth(mel, win_size=43) - smooth(mel, win_size=129) #mid-low frequency component
-#     mel_3 = smooth(mel, win_size=17) - smooth(mel, win_size=43) #mid-high frequency component
-#     mel_4 = mel.squeeze(0) - smooth(mel, win_size=17) #high frequency component (# fine detail / noise)
-#     ###
-    
-#     ### original indices: window sizes=129 (1500ms), 13 (150ms), 5 (50ms), residual
-#     # new indices: window sizes= 129 (1500ms), 43 (500ms), 17 (200ms), residual
-#     components = [mel_1, mel_2, mel_3, mel_4]
-    
-#     # # ===== UNIFY THE COLOR SCALE =====
-#     # all_vals = torch.cat([c.flatten() for c in components])
-#     # vmin = all_vals.min().item()
-#     # vmax = all_vals.max().item()
-#     # print(f"Sample {idx+1}: Color scale range: [{vmin:.2f}, {vmax:.2f}]")
-#     # exit()
-#     plt.figure(figsize=(12, 3*4))
-#     for i, comp in enumerate(components):
-#         plt.subplot(4, 1, i+1)
-#         # plt.imshow(comp.numpy(), aspect='auto', origin='lower', cmap='magma')
-#         if i == 0:
-#             plt.imshow(comp.numpy(), aspect='auto', origin='lower',
-#             cmap='magma', vmin=-10, vmax=4)
-#         elif i == 1:
-#             plt.imshow(comp.numpy(), aspect='auto', origin='lower',
-#             cmap='magma', vmin=-7, vmax=4)
-#         elif i == 2:
-#             plt.imshow(comp.numpy(), aspect='auto', origin='lower',
-#             cmap='magma', vmin=-12, vmax=8)
-#         else:
-#             plt.imshow(comp.numpy(), aspect='auto', origin='lower',
-#             cmap='magma', vmin=-5, vmax=4)
-#         plt.colorbar(format='%+2.0f dB')
-#         plt.ylabel("Frequency bin")
-#         plt.title(f"Sample {idx+1} - {labels[i]}")
-
-#     plt.xlabel("Time frames")
-#     save_path = os.path.join(output_dir, f"{p.parent.name}.png")
-#     plt.tight_layout()
-#     plt.savefig(save_path, dpi=300)
-#     plt.close()
-#     print(f"Saved decomposition figure: {save_path}")
-
-
-#####################################################################################
-
 import torch
 import torch.nn.functional as F
 import numpy as np
@@ -153,19 +26,19 @@ cfg = Config()
 # User Setting
 # =====================================================
 
-# plot_duration_sec = 0 # where is your breakfast
+# where is your breakfast
 
 wav_paths = [
-    "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_1.0/03_src_e--03_ref_e.wav",
-    "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_0.75/03_src_e--03_ref_e.wav",
-    "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_0.5/03_src_e--03_ref_e.wav",
-    "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_0.25/03_src_e--03_ref_e.wav",
-    "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/speaker_split_0.0/03_src_e--03_ref_e.wav",
+    "/.../03_src_e--03_ref_e.wav",
+    "/.../03_src_e--03_ref_e.wav",
+    "/.../03_src_e--03_ref_e.wav",
+    "/.../03_src_e--03_ref_e.wav",
+    "/.../03_src_e--03_ref_e.wav",
 ]
 
 labels = ["Low", "Mid-Low", "Mid-High", "Residual"]
 
-output_dir = "/vol/tensusers8/jzheng/T8_Exp/PSST_2/UUVC/result_demo/decompose_figs"
+output_dir = "/.../decompose_figs"
 os.makedirs(output_dir, exist_ok=True)
 
 
